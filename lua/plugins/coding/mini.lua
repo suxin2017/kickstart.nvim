64ef851a4033d0d5178e9cb76511ffc52acef084
suxin2017 <1107178482@qq.com>
2024-06-21 17:28:16 +0800

    some plugins

--- lua/plugins/coding/mini.lua
+++ lua/plugins/coding/mini.lua
@@ -11,9 +11,21 @@ return {
       return {
         custom_textobjects = {
           F = gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+          t = gen_spec.treesitter { a = '@jsx.outer', i = '@jsx.inner' },
         },
       }
     end,
   },
-  { 'echasnovski/mini.surround', event = 'VeryLazy', version = '*' },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        surrounds = {
+          ['t'] = false,
+        }, -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
---
+++ queries/typescript/textobjects.scm
@@ -0,0 +1,2 @@
+(jsx_element open_tag:(_) (_) @jsx.inner close_tag:(_)) @jsx.outer
+
