28c67c94cf925808bbf67a47e2b2266b1fdd6f65
suxin2017 <1107178482@qq.com>
2024-06-23 10:43:33 +0800

    update

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -2,13 +2,6 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
   'lewis6991/gitsigns.nvim',
   event = 'VeryLazy',
   opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
     on_attach = function(buffer)
       local gs = package.loaded.gitsigns
 
